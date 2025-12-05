b1a0371ec65bce00d891756aa775285f66318d64
Sinn0mbre222 <oruibej0111@g.educaand.es>
2025-12-05 08:55:52 +0100

    RegistroActivity

--- Android/app/src/main/java/com/fireAlert/RegistroActivity.java
+++ Android/app/src/main/java/com/fireAlert/RegistroActivity.java
@@ -1,5 +1,17 @@
 package com.fireAlert;
 
-public class RegistroActivity {
+import android.os.Bundle;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_activity);
+
+
+
+    }
 }
